a4dcd64871a36f76861033f6e9154a825694bb78
Ish Sharma <116168654+IshSharma41@users.noreply.github.com>
2025-06-06 18:18:52 +0530

    Update imports_test.py

--- experiments/imports_test.py
+++ experiments/imports_test.py
@@ -1,1 +1,9 @@
 
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# test_import.py
+from src.preprocessing_modules.modules import mri_harmonization
+print("Success!")
